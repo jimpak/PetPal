aa049faa2e734320ceae829cc9b5749ad11b11fb
jeniehyuks <145646028+jeniehyuks@users.noreply.github.com>
2024-02-05 17:43:46 +0900

    내정보 수정 레이아웃 , 정보저장 모델, 프레그먼트 진행중

--- PetPalApp/app/src/main/java/org/bitc/petpalapp/ui/MyprofileFragment.kt
+++ PetPalApp/app/src/main/java/org/bitc/petpalapp/ui/MyprofileFragment.kt
@@ -1,12 +1,23 @@
 package org.bitc.petpalapp.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import org.bitc.petpalapp.MyApplication
 import org.bitc.petpalapp.R
 import org.bitc.petpalapp.databinding.FragmentMyprofileBinding
+import java.io.File
+import java.util.Date
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +33,7 @@ class MyprofileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var filePath:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +50,34 @@ class MyprofileFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding=FragmentMyprofileBinding.inflate(inflater,container, false)
 
+        val requestLancher=registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode===android.app.Activity.RESULT_OK){
+                Glide
+                    .with(requireActivity())
+                    .load(it.data?.data)
+                    .apply(RequestOptions().override(250,200))
+                    .centerCrop()
+                    .into(binding.profileImageView)
+
+                val cursor = contentResolver.query(it.data?.data as Uri,
+                    arrayOf<String>(MediaStore.Images.Media.DATA), null, null,null);
+                cursor?.moveToFirst().let{
+                    filePath=cursor?.getString(0) as String
+                }
+            }
+        }
+
+        binding.changeProfileImageButton.setOnClickListener{
+
+            val intent= Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*"
+            )
+            requestLancher.launch(intent)
+        }
+
         binding.btnSave.setOnClickListener{
             val nickname=binding.editName.text.toString()
             val username=binding.editNickname.text.toString()
@@ -50,10 +90,46 @@ class MyprofileFragment : Fragment() {
             val boardcount=binding.editBoardcount.text.toString()
 
 
+            val data = mapOf(
+                "email" to MyApplication.email,
+                "nickname" to nickname,
+                "usersname" to username,
+                "pass" to pass,
+                "phone" to phone,
+                "address" to address,
+                "usecount" to usecount,
+                "petcout" to petcount,
+                "boardcount" to boardcount
+            )
+            MyApplication.db.collection("users")
+                .add(data)
+                .addOnSuccessListener {
+                    uploadImage(it.id)
+                }
+                .addOnFailureListener{
+                    Log.d("pgm","data save error",it)
+                }
 
         }
         return binding.root
     }
+
+
+    fun uploadImage(docId:String){
+        //add............................
+        val storage = MyApplication.storage
+        val storageRef = storage.reference
+        val imgRef=storageRef.child("images/${docId}.jpg")
+        val file= Uri.fromFile(File(filePath))
+        imgRef.putFile(file) //파일업로드
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "save ok..", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Log.d("kkang", "file save error", it)
+            }
+    }
+
 
     companion object {
         /**
