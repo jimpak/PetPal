cfa68255db58284ae5b43b8bc437510e83a332fd
eunjujeong1 <49854928+eunjujeong1@users.noreply.github.com>
2024-02-07 14:14:38 +0900

    petstar메인페이지(미완) 등록페이지(수정) 추가

---
+++ PetPalApp/app/src/main/java/org/bitc/petpalapp/ui/petstargram/itemPetstar.kt
@@ -0,0 +1,10 @@
+package org.bitc.petpalapp.ui.petstargram
+
+data class itemPetstar(
+    var docId:String?=null,
+    var email:String?=null,
+    var content:String?=null,
+    var data:String?=null,
+//    var faviruteCount:Int=0,
+//    var favorites:Map<String,Boolean> = HashMap()
+)
