d381ada3142fb34b98e6aa8593bd4ec242a8b9d0
jimpak <jmpak1212@gmail.com>
2024-02-14 09:52:32 +0900

    마이홈에 로그아웃 기능 추가

--- PetPalApp/app/src/main/java/org/bitc/petpalapp/ui/myhome/MyhomeFragment.kt
+++ PetPalApp/app/src/main/java/org/bitc/petpalapp/ui/myhome/MyhomeFragment.kt
@@ -1,8 +1,12 @@
 package org.bitc.petpalapp.ui.myhome
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CalendarView
@@ -16,6 +20,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
+import org.bitc.petpalapp.AuthActivity
 import org.bitc.petpalapp.MyApplication
 import org.bitc.petpalapp.model.ApplicationItem
 import org.bitc.petpalapp.model.Event
@@ -51,6 +56,8 @@ class MyhomeFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+
         calendarView = binding.calendarView
         recyclerView = binding.recyclerView
 //        binding.btnRegiPetsitter.setOnClickListener {
@@ -160,5 +167,34 @@ class MyhomeFragment : Fragment() {
         val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         return sdf.format(calendar.time)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_auth, menu)
+
+        val logoutMenuItem = menu.findItem(R.id.logoutMenuItem)
+
+        if (MyApplication.checkAuth()) {
+            logoutMenuItem.isVisible = true
+        } else {
+            logoutMenuItem.isVisible = false
+        }
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logoutMenuItem -> {
+                MyApplication.auth.signOut()
+                MyApplication.email = null
+                startActivity(Intent(requireContext(), AuthActivity::class.java))
+                requireActivity().finish()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 }
 
