70f3cb731fcbf9075d98ccb8a439628bd0238d1f
eunjujeong1 <49854928+eunjujeong1@users.noreply.github.com>
2024-02-14 11:42:05 +0900

    like 증가 감소(수정)

--- PetPalApp/app/src/main/java/org/bitc/petpalapp/ui/petstargram/DetailFragment.kt
+++ PetPalApp/app/src/main/java/org/bitc/petpalapp/ui/petstargram/DetailFragment.kt
@@ -14,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.toObject
+import org.bitc.petpalapp.model.UserInfo
 import org.bitc.petpalapp.MyApplication
 import org.bitc.petpalapp.R
 import org.bitc.petpalapp.databinding.FragmentPetstargramDetailBinding
@@ -24,14 +26,14 @@ import java.util.Date
 
 class DetailFragment : Fragment() {
     lateinit var _binding: FragmentPetstargramDetailBinding
-    lateinit var docId:String
+    lateinit var docId: String
 
 
-    private val binding get()=_binding!!
+    private val binding get() = _binding!!
 
-    lateinit var filepath:String
-    private var isLiked=false
-   private var likeCount = 0
+    lateinit var filepath: String
+    private var isLiked = false
+    private var likeCount = 0
     private var gooduser: String? = null
 
 
@@ -46,74 +48,95 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding= FragmentPetstargramDetailBinding.inflate(inflater,container,false)
+        _binding = FragmentPetstargramDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        docId=arguments?.getString("docId").toString()
+        docId = arguments?.getString("docId").toString()
 
-        if (docId != null){
-            val docRef=MyApplication.db.collection("petstars").document(docId)
+        if (docId != null) {
+            val docRef = MyApplication.db.collection("petstars").document(docId)
 
-            docRef.get()
-                .addOnSuccessListener {documentSnapsshot->
+
+
+            docRef
+                .get()
+                .addOnSuccessListener { documentSnapsshot ->
                     val item = documentSnapsshot.toObject(PetstargamItem::class.java)
-                    Log.d("email","${item?.email}")
-                    gooduser=item?.gooduser
+                    val userItem=documentSnapsshot.toObject(UserInfo::class.java)
+
+                    Log.d("이건petstaremail", "${item?.email}")
+                    Log.d(
+                        "이건 email ",
+                        "${userItem?.email}"
+                    )
 
                     binding.detailProfile.setText(item?.email)
                     binding.detailContent.setText(item?.content)
-                    likeCount=item?.goodCount!!
-                    if (likeCount>0){
-                        isLiked=true
-                    }else{
+                    likeCount = item?.goodCount!!
+                    gooduser = userItem?.email
+
+                    Log.d("count", "${likeCount}")
+                    Log.d("likeemail","${gooduser}")
+
+                    if (item?.email==userItem?.email) {
+                        if (likeCount > 0) {
+                            isLiked = true
+                        }else{
+                            isLiked=false
+                        }
+                    }else if (item?.email!=userItem?.email){
                         isLiked=false
                     }
-                    if (isLiked){
-                        binding.detailLikeBin.setImageResource(R.drawable.ic_favorite)
-                    }else{
-                        binding.detailLikeBin.setImageResource(R.drawable.ic_favorite_border)
-                    }
-                   binding.detailLikecount.text="Like ${likeCount}"
-                    val imgRef=MyApplication.storage.reference.child("petstarimages/${docId}.jpg")
+
+                            if (isLiked) {
+                                binding.detailLikeBin.setImageResource(R.drawable.ic_favorite)
+                            } else {
+                                binding.detailLikeBin.setImageResource(R.drawable.ic_favorite_border)
+                            }
+                            binding.detailLikecount.text = "Like ${likeCount}"
+
+
+                    val imgRef = MyApplication.storage.reference.child("petstarimages/${docId}.jpg")
                     //프로필 이미지
 
                     //인스타 디테일 이미지
-                    imgRef.downloadUrl.addOnCompleteListener { task->
-                        if (task.isSuccessful){
+                    imgRef.downloadUrl.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
                             Glide
                                 .with(requireContext())
                                 .load(task.result)
-                                .into(binding.detailPetstarImg )
+                                .into(binding.detailPetstarImg)
                         }
                     }
                 }
 
-                .addOnFailureListener { Exception->
-                    Log.d("errrorrrrr","잘못된 다시!!")
+
+                .addOnFailureListener { Exception ->
+                    Log.d("errrorrrrr", "잘못된 다시!!")
                 }
 
             binding.detailLikeBin.setOnClickListener {
                 //클릭시 좋아요 수 증가, 하트 채우기
-                isLiked= !isLiked
+                isLiked = !isLiked
                 if (isLiked) {
                     binding.detailLikeBin.setImageResource(R.drawable.ic_favorite)
                     likeCount++
-                }else if(!isLiked){
+                } else if (!isLiked) {
                     binding.detailLikeBin.setImageResource(R.drawable.ic_favorite_border)
                     likeCount--
 
                 }
-                binding.detailLikecount.text = "Like"+likeCount.toString()
-                Log.d("증가했니????","${likeCount}")
+                binding.detailLikecount.text = "Like" + likeCount.toString()
+                Log.d("증가했니????", "${likeCount}")
 
 
             }
 
-            binding.backPetstar.setOnClickListener{
+            binding.backPetstar.setOnClickListener {
                 likesave()
                 val fragmentManager = requireActivity().supportFragmentManager
 
@@ -122,29 +145,28 @@ class DetailFragment : Fragment() {
             }
 
 
-
-        }else{
-            Log.d("eeeeeeeeeeroro","다시다시")
+        } else {
+            Log.d("eeeeeeeeeeroro", "다시다시")
         }
 
-        val requestLauncher=
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        val requestLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 Glide
                     .with(requireContext())
                     .load(it.data?.data)
-                    .apply(RequestOptions().override(300,300))
+                    .apply(RequestOptions().override(300, 300))
                     .centerCrop()
                     .into(binding.detailPetstarImg)
 
-                val cursor=requireActivity().contentResolver.query(
+                val cursor = requireActivity().contentResolver.query(
                     it.data?.data as Uri, arrayOf<String>(
                         MediaStore.Images.Media.DATA
-                    ),null,null,null
+                    ), null, null, null
                 )
                 cursor?.moveToFirst().let {
-                    filepath=cursor?.getString(0) as String
+                    filepath = cursor?.getString(0) as String
                 }
-                Log.d("확익","filePath:$filepath")
+                Log.d("확익", "filePath:$filepath")
             }
 
     }
@@ -155,20 +177,25 @@ class DetailFragment : Fragment() {
             "email" to MyApplication.email,
             "content" to binding.detailContent.text,
             "data" to dateToString(Date()),
-            "goodCount" to likeCount      )
+            "goodCount" to likeCount,
+        )
 
         MyApplication.db.collection("petstars")
             .document(docId)
             .set(like)
             .addOnSuccessListener {
+
                 Log.d("들어갔니", "likeCount : $likeCount")
-          }
+
+            }
             .addOnFailureListener {
                 Log.d("실패했니", "likeCount : $docId")
             }
     }
 
     }
+
+
 
 
 
